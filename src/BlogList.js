f8ee54fdc876c365959b755e3c51c1cd3a1e901e
heatsynq <izukuzero@gmail.com>
2024-11-23 20:43:17 +0500

    added blog list to page using props

---
+++ src/BlogList.js
@@ -0,0 +1,19 @@
+const BlogList = ({blogs}) => {
+
+    return (  
+        <div className="blog-list">
+            {
+                blogs.map((blog) => {
+                    return(
+                        <div className="blog-preview" key={blog.id}>
+                            <h2>{blog.title}</h2>
+                            <p>Written by: {blog.author}</p>
+                        </div>
+                    )    
+                })
+            }
+        </div>
+    );
+}
+ 
+export default BlogList;
