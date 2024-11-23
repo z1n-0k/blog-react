f6e3a4c2fae11d3c8f19226c5ec414f069bac8c1
heatsynq <izukuzero@gmail.com>
2024-11-23 10:45:43 +0500

    added some blog templates

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState } from 'react'
+
 const Home = () => {
-    return ( 
+
+    const [blogs, setBlogs] = useState([
+        {title: "My new website", body: "lorem ipsum ...", author: 'mario', id:1},
+        {title: "Welcome Party!", body: "lorem ipsum ...", author: 'yoshi', id:2},
+        {title: "Web Dev tool tips", body: "lorem ipsum ...", author: 'mario', id:3}
+    ])   
+
+    return (
         <div className="home">
-            <h2>Home</h2>
+            {
+                blogs.map((blog) => {
+                    return(
+                        <div className="blog-preview" key={blog.id}>
+                            <h2>{blog.title}</h2>
+                            <p>Written by: {blog.author}</p>
+                        </div>
+                    )    
+                })
+            }
         </div>
      );
 }
