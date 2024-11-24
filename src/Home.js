048673e3505d87cb588f434a1f975ea1d20df04f
heatsynq <izukuzero@gmail.com>
2024-11-24 11:27:05 +0500

    replaced Home.js and SelectAuthorFilter.js with chatgpt - better formatted code

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,50 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import SelectAuthorFilter from './SelectAuthorFilter';
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null)
-
-    const authorArray = blogs ? Array.from(
-        new Set(blogs.map((blog) => blog.author))
-    ) : [];
-
+    const [blogs, setBlogs] = useState(null);
     const [selectedAuthor, setSelectedAuthor] = useState("");
+
     const handleAuthorChange = (event) => {
         setSelectedAuthor(event.target.value);
-    }
+    };
+
+    const authorArray = blogs
+        ? Array.from(new Set(blogs.map((blog) => blog.author)))
+        : [];
 
     useEffect(() => {
-        fetch("http://localhost:5000/blogs").then(res => {
-            return res.json();
-        }).then((data) => {
-            setBlogs(data);
-
-        })
-    }, [])
+        fetch("http://localhost:5000/blogs")
+            .then((res) => res.json())
+            .then((data) => setBlogs(data));
+    }, []);
 
     return (
         <div className="home">
-            {authorArray && <SelectAuthorFilter authorArray = {authorArray} selectedAuthor = {selectedAuthor} handleAuthorChange = {handleAuthorChange}/>}
-            {blogs && <BlogList blogs={selectedAuthor ? blogs.filter((blog) => blog.author === selectedAuthor) : blogs}  title={selectedAuthor ? `${selectedAuthor}'s Blogs!` : "All Blogs"}/>}
+            {authorArray && (
+                <SelectAuthorFilter
+                    authorArray={authorArray}
+                    selectedAuthor={selectedAuthor}
+                    handleAuthorChange={handleAuthorChange}
+                />
+            )}
+            {blogs && (
+                <BlogList
+                    blogs={
+                        selectedAuthor
+                            ? blogs.filter((blog) => blog.author === selectedAuthor)
+                            : blogs
+                    }
+                    title={
+                        selectedAuthor
+                            ? `${selectedAuthor}'s Blogs!`
+                            : "All Blogs"
+                    }
+                />
+            )}
         </div>
-     );
-}
+    );
+};
 
 export default Home;
--- src/SelectAuthorFilter.js
+++ src/SelectAuthorFilter.js
@@ -1,18 +1,30 @@
-const SelectAuthorFilter = ({authorArray, selectedAuthor, handleAuthorChange}) => {
+const SelectAuthorFilter = ({ authorArray, selectedAuthor, handleAuthorChange }) => {
     return (
-        <div id = "blog-modifiers"> 
-                <label htmlFor="author-select" id="author-select-label">Choose an author:</label>
-                <select name="author" id="author-select" value={selectedAuthor} onChange={handleAuthorChange}>    
-                <option className= "author-select-option" value="">All Authors</option>
-                    {
-                        authorArray.map((author,index) => (
-                            <option  className= "author-select-option" key={index} value={author}>
-                                {author}
-                            </option>
-                        ))
-                    }
-        </select></div>
-      );
-}
- 
+        <div id="blog-modifiers">
+            <label htmlFor="author-select" id="author-select-label">
+                Choose an author:
+            </label>
+            <select
+                name="author"
+                id="author-select"
+                value={selectedAuthor}
+                onChange={handleAuthorChange}
+            >
+                <option className="author-select-option" value="">
+                    All Authors
+                </option>
+                {authorArray.map((author, index) => (
+                    <option
+                        className="author-select-option"
+                        key={index}
+                        value={author}
+                    >
+                        {author}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
 export default SelectAuthorFilter;
